de3caa3fe016659128948eb477bd418492defc91
Da_Real <musawamanya23@gmail.com>
2024-03-19 12:36:54 +0300

    Your commit message here

---
+++ details.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood details</title>
+</head>
+<body>
+<div class="container-fluid mt-3">
+<h1>Blood Bank Management System</h1>
+
+<div class="column">
+    <div class="col-sm-4 p-3 bg-primary text-white">.col</div>
+    <div class="col-sm-8 p-3 bg-dark text-white">.col</div>
+  </div>
+</div>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,91 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Site</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="jumbotron">
+    <div class="container my-5">
+    <h2 class = "display-3">Welcome to Arua Blood Bank Management System </h2>
+    <p>Arua Blood Bank Management System helps you keep track of the blood supplied by different blood donors </p>
+     
+</div>
+   
+          <img src='images.jpg' class="img-fluid" alt="Hero Image" >  
+        
+      
+<div class="container my-5">
+     <a class="btn btn-primary" href="/bloodbank/login.php" role="button">Log In</a>
+     <p>Don't have an account?</p>
+     <a class="btn btn-primary" href="bloodbank/signup.php" role="button">Sign Up</a>
+     <br>
+     </div>
+      
+     <div class="container my-5">
+     <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Address</th>
+                <th>Created At</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+</div>
+            <?php
+            $servername ="localhost";
+            $username = "root";
+            $password = "";
+            $database = "bloodbank";
+
+            //create connection
+            $connection = new mysqli($servername, $username, $password, $database );
+
+            //check connection
+            if ($connection->connect_error) {
+                die("Connection failed:" . $connection->connect_error);
+            }
+
+            //read all row from database table
+            $sql = "SELECT * FROM donor_details";
+            $result = $connection->query($sql);
+
+            if (!$result) {
+                die("Invalid query: " .$connect->error);
+            }
+
+            //read data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "
+                <tr>
+                <td>$row[id]</td>
+                <td>$row[name]</td>
+                <td>$row[email]</td>
+                <td>$row[phone]</td>
+                <td>$row[address]</td>
+                <td>$row[created_at]</td>
+                <td>
+                    <a class ='btn btn-primary btn-sm' href='/bloodbank/edit.php?id=$row[id]'>Edit</a>
+                    <a class ='btn btn-danger btn-sm' href='/bloodbank/delete.php?id=$row[id]'>Delete</a>
+
+                </td>
+            </tr>
+                ";
+            }
+            ?>
+            
+        </tbody>
+     </table>
+</div>
+    
+</body>
+</html>
